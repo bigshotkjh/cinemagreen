f42738444965a1486d8ff11b37620f889266fe97
bigshotkjh <bigshotkjh@gmail.com>
2024-07-29 10:49:26 +0900

    jihoon

---
+++ src/main/java/com/min/cinemagreen/dto/BlogDTO.java
@@ -0,0 +1,24 @@
+package com.min.cinemagreen.dto;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class BlogDTO {
+  private int blogNo;
+  private String title;
+  private String contents;
+  private int hit;
+  private int userNo;
+  private Date createDt;
+  private Date modifyDt;
+  private String name;
+  private String email;
+}
